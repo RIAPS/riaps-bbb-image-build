0d1f0632b2c63b7f5bb70465fbab5dc0f291a21f
jeholliday <joseph.e.holliday@vanderbilt.edu>
2018-09-24 16:17:31 -0500

    Integrate bbb bootstrap with config

--- riaps-install/base_bbb_bootstrap.sh
+++ riaps-install/base_bbb_bootstrap.sh
@@ -5,8 +5,16 @@ set -e
 # GCC 7, G++ 7, GIT, pkg-config, python3-dev, python3-setuptools
 # pps-tools, libpcap0.8, nettle6, libgnutls30, libncurses5
 
+#contains: rfs_username, release_date
+if [ -f /etc/rcn-ee.conf ] ; then
+	. /etc/rcn-ee.conf
+fi
+if [ -f /etc/oib.project ] ; then
+	. /etc/oib.project
+fi
+
 # Script Variables
-RIAPSAPPDEVELOPER=riaps
+RIAPSAPPDEVELOPER=${rfs_username}
 
 # Script functions
 check_os_version() {
@@ -15,97 +23,44 @@ check_os_version() {
     true
 }
 
-# Install RT Kernel
-rt_kernel_install() {
-    sudo apt update
-    sudo /opt/scripts/tools/update_kernel.sh --ti-rt-kernel --lts-4_14
-    # To make sure the latest overlays are available
-    sudo apt install --only-upgrade bb-cape-overlays
-    echo "installed RT Kernel"
-}
-
 user_func() {
-  if ! id -u $RIAPSAPPDEVELOPER > /dev/null 2>&1; then
-      echo "The user does not exist; setting user account up now"
-      sudo useradd -m -c "RIAPS App Developer" $RIAPSAPPDEVELOPER -s /bin/bash -d /home/$RIAPSAPPDEVELOPER
-      sudo echo -e "riaps\nriaps" | sudo passwd $RIAPSAPPDEVELOPER
-      getent group gpio || sudo groupadd gpio
-      sudo usermod -aG sudo $RIAPSAPPDEVELOPER
-      sudo usermod -aG dialout $RIAPSAPPDEVELOPER
-      sudo usermod -aG gpio  $RIAPSAPPDEVELOPER
-      sudo usermod -aG pwm $RIAPSAPPDEVELOPER
-      sudo -H -u $RIAPSAPPDEVELOPER mkdir -p /home/$RIAPSAPPDEVELOPER/riaps_apps
-      cp etc/sudoers.d/riaps /etc/sudoers.d/riaps
-      echo "created user accounts"
-  fi
-}
+    getent group gpio || sudo groupadd gpio
+    getent group dialout || sudo groupadd dialout
+    getent group pwm || sudo groupadd pwm
 
-# Needed to allow apt-get update to work properly
-rdate_install() {
-    sudo apt-get install rdate -y
-    sudo rdate -n -4 time.nist.gov
-    echo "installed rdate"
-}
+    sudo usermod -aG sudo $RIAPSAPPDEVELOPER
+    sudo usermod -aG dialout $RIAPSAPPDEVELOPER
+    sudo usermod -aG gpio  $RIAPSAPPDEVELOPER
+    sudo usermod -aG pwm $RIAPSAPPDEVELOPER
 
-vim_func() {
-    sudo apt-get install vim -y
-    echo "installed vim"
-}
-
-cmake_func() {
-    sudo apt-get install cmake -y
-    sudo apt-get install byacc flex libtool libtool-bin -y
-    sudo apt-get install autoconf autogen -y
-    sudo apt-get install libreadline-dev -y
-    echo "installed cmake"
-}
-
-htop_install() {
-    sudo apt-get install htop -y
-}
-
-timesync_requirements() {
-    sudo apt-get install linuxptp libnss-mdns gpsd chrony -y
-    sudo apt-get install  libssl-dev libffi-dev -y
-    sudo apt-get install rng-tools -y
-    sudo systemctl start rng-tools.service
-    echo "installed timesync requirements"
+    sudo -H -u $RIAPSAPPDEVELOPER mkdir -p /home/$RIAPSAPPDEVELOPER/riaps_apps
+    cp etc/sudoers.d/riaps /etc/sudoers.d/riaps
+    echo "setup user account"
 }
 
 freqgov_off() {
     touch /etc/default/cpufrequtils
     echo "GOVERNOR=\"performance\"" | tee -a /etc/default/cpufrequtils
     sudo systemctl disable ondemand
-    sudo /etc/init.d/cpufrequtils restart
     echo "setup frequency and governor"
 }
 
+# Install packages that take a long time compiling on the BBBs to minimize user RIAPS installation time
 python_install() {
-    sudo apt-get install python3-pip -y
     sudo pip3 install --upgrade pip
+    echo "upgraded pip"
+
     sudo pip3 install pydevd
-    echo "installed python3 and pydev"
-}
+    echo "installed pydev"
 
-cython_install() {
     sudo pip3 install cython --verbose
+    sudo wget https://raw.githubusercontent.com/fabioz/PyDev.Debugger/master/setup_cython.py -P /usr/local/lib/python3.5/dist-packages/
+    sudo python3 /usr/local/lib/python3.5/dist-packages/setup_cython.py build_ext --inplace
     echo "installed cython"
-}
 
-curl_func() {
-    sudo apt install curl -y
-    echo "installed curl"
+    sudo pip3 install 'paramiko==2.4.1' 'cryptography==2.1.4' --verbose
+    echo "installed paramiko and cryptography"
 }
-
-boost_install() {
-    sudo apt-get install libboost-all-dev -y
-    echo "installed boost"
-}
-
-# MM TODO: not sure if this will be needed, here in case it is
-#libsoc_install() {
-#    sudo apt-get install libsoc2
-#}
 
 watchdog_timers() {
     echo " " >> /etc/sysctl.conf
@@ -123,7 +78,7 @@ quota_install() {
     echo "setup quotas"
 }
 
-splash_screen_update() {
+motd_update() {
     echo "################################################################################" > motd
     echo "# Acknowledgment:  The information, data or work presented herein was funded   #" >> motd
     echo "# in part by the Advanced Research Projects Agency - Energy (ARPA-E), U.S.     #" >> motd
@@ -132,34 +87,15 @@ splash_screen_update() {
     echo "# those of the United States Government or any agency thereof.                 #" >> motd
     echo "################################################################################" >> motd
     sudo mv motd /etc/motd
-    # Issue.net
-    echo "Ubuntu 18.04.1 LTS" > issue.net
-    echo "" >> issue.net
-    echo "rcn-ee.net console Ubuntu Image 2018-08-10">> issue.net
-    echo "">> issue.net
-    echo "Support/FAQ: http://elinux.org/BeagleBoardUbuntu">> issue.net
-    echo "">> issue.net
-    echo "default username:password is [riaps:riaps]">> issue.net
-    sudo mv issue.net /etc/issue.net
-    echo "setup splash screen"
+    echo "setup motd screen"
 }
 
 setup_hostname() {
     cp usr/bin/set_unique_hostname /usr/bin/set_unique_hostname
     cp etc/systemd/system/sethostname.service /etc/systemd/system/.
-    systemctl daemon-reload
-    systemctl start sethostname.service
-    systemctl enable sethostname.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable sethostname.service
     echo "setup hostname"
-}
-
-
-setup_peripherals() {
-    getent group gpio ||groupadd gpio
-    getent group dialout ||groupadd dialout
-    getent group pwm ||groupadd pwm
-
-    echo "setup peripherals - gpio, uart, and pwm"
 }
 
 setup_network() {
@@ -177,13 +113,6 @@ setup_network() {
     echo "replaced resolv.conf"
 }
 
-# Install security packages that take a long time compiling on the BBBs to minimize user RIAPS installation time
-security_pkg_install() {
-    echo "add security packages"
-    sudo pip3 install 'paramiko==2.4.1' 'cryptography==2.1.4' --verbose
-    echo "security packages setup"
-}
-
 # This function requires that bbb_initial.pub from https://github.com/RIAPS/riaps-integration/blob/master/riaps-x86runtime/bbb_initial_keys/id_rsa.pub
 # be placed on the bbb as this script is run
 setup_ssh_keys() {
@@ -198,8 +127,6 @@ setup_ssh_keys() {
 }
 
 setup_riaps_repo() {
-    sudo apt-get install software-properties-common apt-transport-https -y
-
     # Add RIAPS repository
     echo "add repo to sources"
     sudo add-apt-repository -r "deb [arch=armhf] https://riaps.isis.vanderbilt.edu/aptrepo/ bionic main" || true
@@ -215,25 +142,13 @@ setup_riaps_repo() {
 
 # Start of script actions
 check_os_version
-rt_kernel_install
 user_func
-rdate_install
-vim_func
-cmake_func
-htop_install
-timesync_requirements
-#freqgov_off
+freqgov_off
 python_install
-cython_install
-curl_func
-boost_install
-#libsoc_install
 watchdog_timers
 quota_install $RIAPSAPPDEVELOPER
-splash_screen_update
+motd_update
 setup_hostname
-setup_peripherals
 setup_network
-security_pkg_install
 setup_ssh_keys $RIAPSAPPDEVELOPER
 #setup_riaps_repo
