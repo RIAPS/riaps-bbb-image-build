f94aeb904b9486d25151531aa0652dd0431aa17c
Joey Holliday <joeyholliday77@gmail.com>
2019-01-21 14:07:01 -0600

    Move back to LTS kernel

--- build.sh
+++ build.sh
@@ -20,6 +20,6 @@ git apply ../omap-image-builder.patch
 # Build image for BBB
 IMG=$(\ls deploy)
 cd deploy/$IMG
-sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone
+sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone --rootfs_label rootfs --enable-cape-universal
 zip $IMG.zip *.img
 mv *.zip ../../../
