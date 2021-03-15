a6d63629c2c09452f9f6366574a79db1b7b74e35
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2021-03-14 22:03:21 -0500

    Installed qemu 5.2.0 on build machine, trying it with full script

--- riaps.sh
+++ riaps.sh
@@ -6,5 +6,5 @@ export LC_ALL=C
 
 cd /opt/riaps-install
 /bin/bash elinux.sh
-#MM TODO: remove for now to determine if kernel works
-#/bin/bash base_bbb_bootstrap.sh
+#MM TODO: remove for now to determine if kernel works - trying with newer qemu (03/14/21)
+/bin/bash base_bbb_bootstrap.sh
