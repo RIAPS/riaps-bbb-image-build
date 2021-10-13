b32e6af0c545887901a834e079f75833a5ef92f0
MMetelko <mary.a.metelko@vanderbilt.edu>
2021-10-13 13:59:35 -0500

    Update conf file and build to match rcn-ee.net-console-ubuntu-focal-v5.10-ti-armhf.conf

--- build.sh
+++ build.sh
@@ -21,6 +21,6 @@ git apply ../omap-image-builder.patch
 # Build image for BBB
 IMG=$(\ls deploy)
 cd deploy/$IMG
-sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone --distro-bootloader
+sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone --distro-bootloader --rootfs_label rootfs --enable-cape-universal
 zip $IMG.zip *.img
 mv *.zip ../../../
