8cedd20278d022ce05b4f470c7acbeabc84dff51
MMetelko <mary.a.metelko@vanderbilt.edu>
2021-10-13 09:35:26 -0500

    rootfs_label is a duplication of the default, so remove found comment in wrong place

--- build.sh
+++ build.sh
@@ -21,6 +21,6 @@ git apply ../omap-image-builder.patch
 # Build image for BBB
 IMG=$(\ls deploy)
 cd deploy/$IMG
-sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone --distro-bootloader --rootfs_label rootfs
+sudo ./setup_sdcard.sh --img-4gb $IMG --dtb beaglebone --distro-bootloader
 zip $IMG.zip *.img
 mv *.zip ../../../
