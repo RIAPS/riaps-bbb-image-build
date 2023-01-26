dde42b1c21fe972432f4c1a1e71f42ff65a23977
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2023-01-26 17:01:33 -0600

    Build a Debian Bullseye version of BBB image

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ git submodule foreach git reset --hard # Reset any changes to submodules
 rm -rf omap-image-builder/deploy/      # Remove any existing builds
 
 # Copy RIAPS files into omap-image-builder repo
-#MM - cp riaps-bionic.conf omap-image-builder/configs/
-cp riaps-focal.conf omap-image-builder/configs/
+#cp riaps-focal.conf omap-image-builder/configs/
+cp riaps-debian-bullseye-console-v5.10-ti-armhf.conf omap-image-builder/configs/
 cp riaps_setup.sh omap-image-builder/target/chroot/
 cp riaps.sh omap-image-builder/target/chroot/
 cp -r riaps-install omap-image-builder/
@@ -16,7 +16,8 @@ cp -r riaps-install omap-image-builder/
 # Build ubuntu filesystem
 cd omap-image-builder
 git apply ../omap-image-builder.patch
-./RootStock-NG.sh -c riaps-focal
+#./RootStock-NG.sh -c riaps-focal
+./RootStock-NG.sh -c riaps-debian-bullseye-console-v5.10-ti-armhf
 
 # Build image for BBB
 IMG=$(\ls deploy)
