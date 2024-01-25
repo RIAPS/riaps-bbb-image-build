596c9e568291343c5720d4b139bd8b7e15a16944
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2024-01-25 10:57:03 -0600

    Add libzmq5 separately so that it is not removed when libzmq3-dev is removed

--- riaps-install/install_scripts/apt_pkg_installs.sh
+++ riaps-install/install_scripts/apt_pkg_installs.sh
@@ -6,6 +6,7 @@ zmq_draft_apt_install() {
     wget -O- https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-draft/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/zeromq-archive-keyring.gpg >/dev/null
     sudo echo "deb [arch=armhf signed-by=/usr/share/keyrings/zeromq-archive-keyring.gpg] http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-draft/xUbuntu_22.04/ ./" >> /etc/apt/sources.list.d/zeromq.list
     sudo apt-get update
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install libzmq5 -y
     DEBIAN_FRONTEND=noninteractive sudo apt-get install libzmq3-dev -y
     echo ">>>>> installed libzmq with draft APIs"
 }
